f96b119e0827b4c999c2caa60e91b71423c7022c
hphuonggg <karmakute16@gmail.com>
2023-04-20 15:02:10 +0700

    finish basic step

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -35,6 +35,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -61,6 +62,8 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -89,7 +92,9 @@ namespace BulkyWeb.Controllers
             }
              _db.Categories.Remove(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Index");
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
             
         }
     }
